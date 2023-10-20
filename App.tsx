03b938fbaaaf5e39ec6042d2f3acbd4b4bb33935
Jasim Khan <jasimkhankotaielectronics@gmail.com>
2023-10-21 04:57:52 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import AppMain from './src/AppMain';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return <AppMain />;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
